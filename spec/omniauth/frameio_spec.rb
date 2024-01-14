71ed9327e0569af8f2e83ebb69beac0c605a150c
Victor Holl <v_h@me.com>
2024-01-14 17:54:24 +0100

    gem new + summary

---
+++ spec/omniauth/frameio_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Omniauth::Frameio do
+  it "has a version number" do
+    expect(Omniauth::Frameio::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
